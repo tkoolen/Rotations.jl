244aef4888a36472c42806665f5dfc35d548a5af
Andy Ferris <andy.ferris@roames.com.au>
2016-08-01 11:39:15 +1000

    Added 0.5 test framework and type-stability to mean()

--- src/mean.jl
+++ src/mean.jl
@@ -13,17 +13,17 @@ Function to find the mean of a set of rotations.
 
 Args:
 
-    qvec    - Vector of `Quat`s (or any other `Rotation`)
+    qvec    - Vector of `Rotation`s
 
     method  - if 0 (default), the returned rotation minimizes sum_i=1:n (||R(Qi) - R(Qbar)||)  with || the Frobenius norm, or
               equivalently minimizes sum_i=1:n (sin(phi_i / 2))^2, where phi_i is the angle of Qe_i, with  Qbar = Q_i x Qe_i
 
-            - if 1, the returned quaternian minimizes sum_i=1:n (phi_i ^2) where phi_i is the angle of Qe_i, with  Qbar = Q_i x Qe_i
+            - if 1, the returned rotation minimizes sum_i=1:n (phi_i ^2) where phi_i is the angle of Qe_i, with  Qbar = Q_i x Qe_i
               this should be about the same as method == 0 if phi_i are all small (note: still not implemented)
 """
 function Base.mean{T}(qvec::AbstractVector{Quat{T}}, method::Integer = 0)
     if (method == 0)
-        M = @MArray zeros(4, 4)
+        M = zeros(4, 4)
         for i = 1:length(qvec)
             q = qvec[i]
             Qi = @SVector [q.w, q.x, q.y, q.z]  # convert types to ensure we don't get quaternion multiplication
