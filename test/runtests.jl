378661a56285481a2ed9fa79ca031b1d7a0c2bab
Tim Holy <tim.holy@gmail.com>
2016-10-10 06:27:09 -0500

    Test that no ambiguities exist

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,11 @@ using Base.Test
 using Rotations
 using StaticArrays
 
+# Check that there are no ambiguities beyond those present in StaticArrays
+ramb = detect_ambiguities(Rotations, Base, Core)
+samb = detect_ambiguities(StaticArrays, Base, Core)
+@test isempty(setdiff(ramb, samb))
+
 # TODO test mean()
 
 include("2d.jl")
