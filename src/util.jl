42d0b8f5723175a3bf04ec5010cac8adbd53a825
Twan Koolen <koolen.twan@gmail.com>
2017-01-16 22:57:24 -0500

    Address comments: add @inbounds.

--- src/util.jl
+++ src/util.jl
@@ -11,11 +11,11 @@ function perpendicular_vector(vec::SVector{3})
     # find indices of the two elements of vec with the largest absolute values:
     absvec = abs(vec)
     ind1 = indmax(absvec) # index of largest element
-    absvec2 = @SVector [ifelse(i == ind1, typemin(T), absvec[i]) for i = 1 : 3] # set largest element to typemin(T)
+    @inbounds absvec2 = @SVector [ifelse(i == ind1, typemin(T), absvec[i]) for i = 1 : 3] # set largest element to typemin(T)
     ind2 = indmax(absvec2) # index of second-largest element
 
     # perp[ind1] = -vec[ind2], perp[ind2] = vec[ind1], set remaining element to zero:
-    perpind1 = -vec[ind2]
-    perpind2 = vec[ind1]
+    @inbounds perpind1 = -vec[ind2]
+    @inbounds perpind2 = vec[ind1]
     perp = @SVector [ifelse(i == ind1, perpind1, ifelse(i == ind2, perpind2, zero(T))) for i = 1 : 3]
 end
